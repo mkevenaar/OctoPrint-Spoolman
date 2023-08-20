8b1ffd0a136097991c80cd40e292cdfdf405292f
Maurice Kevenaar <maurice@kevenaar.name>
2023-08-20 11:10:46 +0200

    (build) setup repo

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # OctoPrint-Spoolman
-OctoPrint companion plugin to update Spoolman
+
+**TODO:** Describe what your plugin does.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
+or manually using this URL:
+
+    https://github.com/mkevenaar/OctoPrint-Spoolman/archive/main.zip
+
+**TODO:** Describe how to install your plugin, if more needs to be done than just installing it via pip or through
+the plugin manager.
+
+## Configuration
+
+**TODO:** Describe your plugin's configuration options (if any).
---
+++ octoprint_Spoolman/static/js/Spoolman.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Spoolman
+ *
+ * Author: Maurice Kevenaar
+ * License: MIT
+ */
+$(function() {
+    function SpoolmanViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: SpoolmanViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_Spoolman, #tab_plugin_Spoolman, ...
+        elements: [ /* ... */ ]
+    });
+});
